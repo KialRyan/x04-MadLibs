56864aa0c7420a589f3abbb1452b5e68f7b017a5
KialRyan <kleiteryan@mail.csuchico.edu>
2018-01-31 20:20:45 -0800

    Asks for responses beore filling in a Madlib

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,25 @@
-//Authors: 
+//Authors:Kial Leite Ryan 
 #include <iostream>
 #include <string>
-
+using namespace std;
 int main()
 {
+  string color;
+  string name;
+  int number;
+  char window ;
+  cout<<"Pick a color" <<endl;
+  cin>> color;
+  cout<<"Choose a name" <<endl;
+  cin>> name;
+  cout<<"Pick a number"<<endl;
+  cin>> number;
+  cout<<"Pick a symbol"<<endl;
+  cin>> window;
+  cout<<"There is a "<< color<< " car. It is owned by "<< name <<endl;
+  cout<<"and the license plate number is "<< number; cout<<"."  <<endl;
+  cout<<"It had a symbol on the back window of the car that looked like "<< window << endl;
+  cout<<"Isn't that neat?"<<endl;
+return 0;
 
-  return 0;
 }
